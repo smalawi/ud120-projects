202d6f10fc4f333718146f9abe55893b2d06958a
smalawi <sayedtwo@mac.com>
2017-08-02 18:37:09 +0300

    finished outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,15 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    with_errors = []
+    for pred, age, act in zip(predictions, ages, net_worths):
+        with_errors.append((age[0], act[0], (pred[0] - act[0])**2))
+    sorted_data = sorted(with_errors, key=lambda t: t[2], reverse=True)
 
+    for i in range(len(sorted_data) / 10, len(sorted_data)):
+        cleaned_data.append(sorted_data[i])
+
+    print len(cleaned_data)
     
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,15 +26,14 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-
-
-
-
-
-
-
+print reg.coef_
+print reg.intercept_
+print reg.score(ages_test, net_worths_test)
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -69,6 +68,8 @@ if len(cleaned_data) > 0:
     try:
         reg.fit(ages, net_worths)
         plt.plot(ages, reg.predict(ages), color="blue")
+        print reg.coef_
+        print reg.score(ages_test, net_worths_test)
     except NameError:
         print "you don't seem to have regression imported/created,"
         print "   or else your regression object isn't named reg"
