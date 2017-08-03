6d01db8afb964c61ace4922106fb64e190420232
smalawi <sayedtwo@mac.com>
2017-08-03 10:49:28 +0300

    finished k means

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -43,11 +43,24 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
+min_eso = 99999999999
+max_eso = 0
+for entry in data_dict:
+    eso = data_dict[entry]["salary"]
+    if eso != "NaN":
+        if eso < min_eso:
+            min_eso = eso
+        if eso > max_eso:
+            max_eso = eso
+
+print min_eso
+print max_eso
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
 features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
@@ -58,15 +71,17 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans
 
-
+clu = KMeans(n_clusters=2).fit(finance_features)
+pred = clu.predict(finance_features)
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
