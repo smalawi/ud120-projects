a031acfbcd7d1b293c81a47f0b7f47499bb4ff64
smalawi <sayedtwo@mac.com>
2017-09-03 15:27:36 -0400

    add features

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -12,8 +12,26 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', 'salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees',
-'to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']  # You will need to use more features
+features_list = ['poi',
+                 'salary',
+                 'deferral_payments',
+                 'total_payments',
+                 'loan_advances',
+                 'bonus',
+                 'restricted_stock_deferred',
+                 'deferred_income',
+                 'total_stock_value',
+                 'expenses',
+                 'exercised_stock_options',
+                 'other',
+                 'long_term_incentive',
+                 'restricted_stock',
+                 'director_fees',
+                 'to_messages',
+                 'from_poi_to_this_person',
+                 'from_messages',
+                 'from_this_person_to_poi',
+                 'shared_receipt_with_poi']
 email_features = ['to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -27,6 +45,12 @@ data_df = pd.DataFrame.from_dict(data_dict, orient='index')
 
 data_df.replace('NaN', np.NaN, inplace=True)
 means = data_df.mean()
+medians = data_df.median()
+
+# for feature in features_list:
+#     if feature not in email_features:
+#         if feature != 'poi':
+#             features_list.remove(feature)
 
 for person in data_dict:
     for feature in features_list:
@@ -34,14 +58,30 @@ for person in data_dict:
             if feature in email_features:
                 data_dict[person][feature] = means[feature]
             else:
-                data_dict[person][feature] = 0
+                data_dict[person][feature] = medians[feature]
 ### Task 3: Create new feature(s)
+features_list.append('fraction_from_poi')
+features_list.append('fraction_to_poi')
+features_list.append('fraction_restricted_stock')
+# features_list.append('fraction_deferred_stock')
+# features_list.append('bonus_salary')
+for person in data_dict:
+    data_dict[person]['fraction_from_poi'] = float(data_dict[person]['from_poi_to_this_person']) / data_dict[person]['from_messages']
+    data_dict[person]['fraction_to_poi'] = float(data_dict[person]['from_this_person_to_poi']) / data_dict[person]['to_messages']
+    data_dict[person]['fraction_restricted_stock'] = float(data_dict[person]['restricted_stock']) / data_dict[person]['total_stock_value']
+    # data_dict[person]['fraction_deferred_stock'] = float(data_dict[person]['restricted_stock_deferred']) / data_dict[person]['restricted_stock']
+    # data_dict[person]['bonus_salary'] = data_dict[person]['bonus'] * 1.0 / data_dict[person]['salary']
+
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+# from sklearn.feature_selection import SelectPercentile, chi2
+# selector = SelectPercentile(score_func=chi2, k=10)
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -55,19 +95,19 @@ from sklearn.ensemble import RandomForestClassifier
 # from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
 from sklearn.tree import DecisionTreeClassifier
-clf = DecisionTreeClassifier(criterion="gini",
-                             splitter="best",
-                             max_depth=2,
-                             min_samples_split=2,
-                             class_weight='balanced')
+# clf = DecisionTreeClassifier(criterion="gini",
+#                              splitter="best",
+#                              max_depth=2,
+#                              min_samples_split=2,
+#                              class_weight='balanced')
 
 dt = DecisionTreeClassifier(random_state=42)
 metrics = 'precision'
 params = {
     'criterion'         : ['gini', 'entropy'],
     'splitter'          : ['best', 'random'],
-    'max_depth'         : [2, 10, 20, 30],
-    'min_samples_split' : [2, 4, 6],
+    'max_depth'         : [2, 10, 20, 30, 40],
+    'min_samples_split' : [2, 4, 6, 8],
     'class_weight'      : [None, 'balanced']
 }
 # clf = AdaBoostClassifier(class_weights='balanced')
